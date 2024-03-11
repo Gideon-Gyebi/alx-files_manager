dcf9007a60976b5d3398e89f68aadaa97e39374e
Gideon-Gyebi <125477741+Gideon-Gyebi@users.noreply.github.com>
2024-03-11 22:43:00 +0000

    Update redis.js

---
+++ utils/redis.js
@@ -0,0 +1,45 @@
+// Task 0. Redis utils:
+import { createClient } from 'redis';
+import { promisify } from 'util';
+
+// class that defines methods for commonly used redis commands
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+    this.client.on('error', (error) => {
+      console.log(`Redis client not connected to server: ${error}`);
+    });
+  }
+
+  // checking connection status and report
+  isAlive() {
+    if (this.client.connected) {
+      return true;
+    }
+    return false;
+  }
+
+  // getting value for given key from redis server
+  async get(key) {
+    const redisGet = promisify(this.client.get).bind(this.client);
+    const value = await redisGet(key);
+    return value;
+  }
+
+  // setting key value pair to redis server
+  async set(key, value, time) {
+    const redisSet = promisify(this.client.set).bind(this.client);
+    await redisSet(key, value);
+    await this.client.expire(key, time);
+  }
+
+  // del key vale pair from redis server
+  async del(key) {
+    const redisDel = promisify(this.client.del).bind(this.client);
+    await redisDel(key);
+  }
+}
+
+const redisClient = new RedisClient();
+
+module.exports = redisClient;
